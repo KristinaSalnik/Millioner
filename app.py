d594979d56053c6d97fbebaaf02a1a9ed648ec25
Кристина Сальник <salnikkristina@yandex.ru>
2023-09-20 19:02:59 +0700

    «Книги

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def start():
 def menu():
     return """
 <!doctype html>
-<html> c
+<html>
     <head>
         <title> Сальник Кристина Андреевна, лабораторная 1</title>
     </head>
@@ -180,4 +180,16 @@ def example():
         {'name': 'мандарины', 'price':95},
         {'name': 'манго', 'price':312}
         ]
-    return render_template('example.html', name=name, course=course, group=group, fruits=fruits)
+    Book = [
+        {'name': 'Пушкин А.С.', 'book_name': 'Метель', 'genre': 'драмма', 'num': 300},
+        {'name': 'Булгаков', 'book_name': 'Мастер и Маргарита', 'genre': 'драмма', 'num': 3980},
+        {'name': 'Пушкин А.С.', 'book_name': 'Метель', 'genre': 'драмма', 'num': 676},
+        {'name': 'Грибоедов', 'book_name': 'Горе от ума', 'genre': 'драмма', 'num': 300},
+        {'name': 'Замятин', 'book_name': 'Мы', 'genre': 'классика', 'num': 67},
+        {'name': 'Достоевский', 'book_name': 'Идиот', 'genre': 'драмма', 'num': 3333},
+        {'name': 'Драйзер', 'book_name': 'Финансист', 'genre': 'драмма', 'num': 3650},
+        {'name': 'Фаулз', 'book_name': 'Коллекционер', 'genre': 'юмор', 'num': 360},
+        {'name': 'Пушкин А.С.', 'book_name': 'Метель', 'genre': 'драмма', 'num': 35780},
+        {'name': 'Пушкин А.С.', 'book_name': 'Метель', 'genre': 'драмма', 'num': 389}
+    ]
+    return render_template('example.html', name=name, course=course, group=group, fruits=fruits, Book=Book)
