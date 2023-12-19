11ec4bdbe5c90245c83ca1f7f0aa8d634bdf8c9c
Кристина Сальник <salnikkristina@yandex.ru>
2023-12-19 21:55:03 +0700

    laba8

--- lab1.py
+++ lab1.py
@@ -43,6 +43,9 @@ def menu():
                 <li>
                     <a href="/lab7/" target="_blank" >Лабораторная 7</a>
                 </li>
+                <li>
+                    <a href="/lab8/" target="_blank" >Лабораторная 8</a>
+                </li>
             </ul>
 
         <footer>
